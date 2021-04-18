83cca326d95246b6f5c521b1056cbe685da9bf11
Uchral <uchral10@gmail.com>
2021-04-18 19:35:46 +0800

    Shoonii zurag garj irdeg bolloo

--- app.js
+++ app.js
@@ -8,14 +8,18 @@ var scores = [0, 0];
 var roundScore = 0;
 
 //shoonii ali tal buusnig hadgalah huvisagch
-var dice = Math.floor( Math.random() * 6)+ 1;
 
-//<div class="player-score" id="score-0">43</div>
-window.document.querySelector('#score-0').textContent = 0;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('.dice').style.display = 'none';
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
-console.log('shoo : ' + dice);
+var diceDom = document.querySelector('.dice');
+diceDom.style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function (){
+    var diceNumber = Math.floor( Math.random() * 6)+ 1;
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + diceNumber+'.png';
+});
 
