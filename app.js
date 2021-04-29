826899bcea685ec6097aeb482b00baa1f4d3f881
Uchral <uchral10@gmail.com>
2021-04-29 16:19:20 +0800

    1 buuh uyd toglogch solij haragddag bolloo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Toglogch haruulah 0 1
-var activePlayer = 1;
+var activePlayer = 0;
 
 //toglogchiin tsugluulsan onoog hadgalah 
 var scores = [0, 0];
@@ -18,8 +18,34 @@ var diceDom = document.querySelector('.dice');
 diceDom.style.display = 'none';
 
 document.querySelector('.btn-roll').addEventListener('click', function (){
+    //1-6 dotor too gargana
     var diceNumber = Math.floor( Math.random() * 6)+ 1;
+    
+    //shooni zurag gargaj irne
     diceDom.style.display = 'block';
+    
+    //buusan toond hargalzah shooni zuragig gargana.
     diceDom.src = 'dice-' + diceNumber+'.png';
-});
+    
+    //1 ees ylgaatai too buuval toglogchiin eeljiin toog nemegduulne.
+    if(diceNumber !== 1){
 
+        roundScore = roundScore + diceNumber;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+        
+        
+    } else {
+        roundScore = 0;
+        document.getElementById('current-' + activePlayer).textContent = 0;
+        //1 buusan uyd toglogchiin eelj solino
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer =0);
+        
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector(".player-1-panel").classList.toggle("active");
+        diceDom.style.display = 'none';
+        }
+    });
+    // document.querySelector("btn-hold").addEventListener('click',function(){
+    //     document.getElementById('score-' + activePlayer).textContent = roundScore;
+    // })
+    
