341894acf3c4050eb8b31eda2bd1e6bd5487ae05
Uchral <uchral10@gmail.com>
2021-05-03 13:02:25 +0800

    winner shalgardag bolloo

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var activePlayer = 0;
 
 //toglogchiin tsugluulsan onoog hadgalah 
 var scores = [0, 0];
-
 //toglogchiin eeljind tsugluulah onoo
 var roundScore = 0;
 
@@ -35,17 +34,28 @@ document.querySelector('.btn-roll').addEventListener('click', function (){
         
         
     } else {
+        switchToNextPlayer();
+        }
+    });
+    document.querySelector(".btn-hold").addEventListener('click',function(){
+        scores[activePlayer] = scores[activePlayer] + roundScore;
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+        
+        if(scores[activePlayer]>= 10){
+            document.getElementById('name-'+activePlayer).textContent = "Winner!!!";
+            document.querySelector('.player-'+ activePlayer +'-panel').classList.add('winner');
+            document.querySelector('.player-'+ activePlayer +'-panel').classList.remove('active');
+        } else  {
+            switchToNextPlayer();
+        }
+        
+    });
+    
+    function switchToNextPlayer(){
         roundScore = 0;
         document.getElementById('current-' + activePlayer).textContent = 0;
-        //1 buusan uyd toglogchiin eelj solino
-        activePlayer === 0 ? (activePlayer = 1) : (activePlayer =0);
-        
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector(".player-1-panel").classList.toggle("active");
         diceDom.style.display = 'none';
-        }
-    });
-    // document.querySelector("btn-hold").addEventListener('click',function(){
-    //     document.getElementById('score-' + activePlayer).textContent = roundScore;
-    // })
-    
+    }
